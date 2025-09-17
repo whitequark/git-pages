82f24d5508897c8703eb79f8727226ee5424530d
Catherine <whitequark@whitequark.org>
2025-09-17 18:31:38 +0000

    Log successful health check.

--- src/health.go
+++ src/health.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func ServeHealth(w http.ResponseWriter, r *http.Request) {
+	log.Println("health: ok")
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintln(w, "ok")
 }
