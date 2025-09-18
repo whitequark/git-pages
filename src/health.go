2ca9a4c244f728ec47751b603df24371378f46ae
Catherine <whitequark@whitequark.org>
2025-09-18 01:40:51 +0000

    Revert "Log successful health check."
    This reverts commit 82f24d5508897c8703eb79f8727226ee5424530d.

--- src/health.go
+++ src/health.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 )
 
 func ServeHealth(w http.ResponseWriter, r *http.Request) {
-	log.Println("health: ok")
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintln(w, "ok")
 }
