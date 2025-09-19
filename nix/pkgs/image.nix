da604215c14b630a315a96ca96ca70e318161e5d
Catherine <whitequark@whitequark.org>
2025-09-19 21:58:38 +0000

    Adapt Caddyfile during the build.
    Until https://github.com/ss098/certmagic-s3/pull/25 is merged, this
    also uses a fork of certmagic-s3.

--- nix/pkgs/image.nix
+++ nix/pkgs/image.nix
@@ -15,10 +15,10 @@ let
   caddy' =
     (caddy.withPlugins {
       plugins = [
-        "github.com/ss098/certmagic-s3@v0.0.0-20250808023250-9788b7231c87"
+        "github.com/ss098/certmagic-s3=github.com/whitequark/certmagic-s3@v0.0.0-20250919212902-21ac26c15951"
       ];
 
-      hash = "sha256-jZer6cBnE2Vo5/kMG+1vZBwWY8P/V1Lb33TA3Suz4pI=";
+      hash = "sha256-Mx+jSvjNt6y7mK4aP/YERmCPWx5UTGTyH2dbXUJ9+UY=";
     }).overrideAttrs
       (oldAttrs: {
         buildInputs = with pkgsStatic; [
@@ -49,7 +49,7 @@ dockerTools.buildImage {
     mkdir /bin
 
     cp ${self}/config.toml.example /app/config.toml
-    cp ${self}/conf/Caddyfile /app/Caddyfile
+    ${caddy'}/bin/caddy adapt --config ${self}/conf/Caddyfile >/app/caddy.json
     cp ${self}/conf/supervisord.conf /app/supervisord.conf
 
     cp ${caddy'}/bin/caddy /bin/caddy
