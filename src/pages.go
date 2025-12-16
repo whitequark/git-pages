whitequark/git-pages

commit 9c6f735df0c6a92ba712a0d11383403a35c12491

Fix loss of context in POST handler.
This caused the principal to not be available when creating the new
audit record.

src/pages.go
@@ -762,7 +762,7 @@ func postPage(w http.ResponseWriter, r *http.Request) error {
 		result := UpdateFromRepository(ctx, webRoot, repoURL, auth.branch)
 		resultChan <- result
 		observeSiteUpdate("webhook", &result)
-	}(context.Background())
+	}(r.Context())
 
 	var result UpdateResult
 	select {

commit 79407ba406275defbaca092a52de116322767418

Fix timeout bug introduced in commit 9c6f735d.
This bug would cause POST hooks triggered for large repositories to
silently fail.

We need the update context to have the principal (which is tied to
the HTTP request), but not the cancellation (which is also tied to
the HTTP request and is triggered once the request is done either way).

src/pages.go
@@ -770,7 +770,7 @@ func postPage(w http.ResponseWriter, r *http.Request) error {
 		result := UpdateFromRepository(ctx, webRoot, repoURL, auth.branch)
 		resultChan <- result
 		observeSiteUpdate("webhook", &result)
-	}(r.Context())
+	}(context.WithoutCancel(r.Context()))
 
 	var result UpdateResult
 	select {